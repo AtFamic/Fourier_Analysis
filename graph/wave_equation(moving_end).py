4d025c99fbd2b31ca000d38630570faf19b5cd93
atfam <2115106u@g.hit-u.ac.jp>
2018-02-16 11:46:09 -0600

    [update] stuff about numpy

---
+++ graph/wave_equation(moving_end).py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+plt.style.use("dark_background")
+fig = plt.figure()
+fig.set_dpi(100)
+ax1 = fig.add_subplot(1,1,1)
+
+x0 = np.linspace(0,20,1000)
+
+t0 = 0
+
+dt = 0.05
+# 時間間隔
+
+c = 2
+
+
+a = []
+
+def y(x,t):
+    result =x
+    result[np.where(c*(t-1)<x)] = np.sin(np.pi*(t-x[np.where(c*(t-1)<x)]/c))
+    if np.where(x<c*t):
+        np.sin(np.pi * (t - x[np.where(x<c*t)] / c))
+    if np.where(x<c*(t-1)):
+        0
+    if np.where(c*t<x):
+        0
+
+for i in x0:
+    value = y(x0,t0)
+    t0 = t0 + dt
+    a.append(value)
+
+k = 0
+
+def _update_plot(i):
+    global k
+    x = a[k]
+    k += 1
+    ax1.clear()
+    plt.plot(x0,x)
+    plt.grid(True)
+    plt.ylim([-1,4])
+    plt.xlim([0,20])
+
+
+
+ani = animation.FuncAnimation(fig, _update_plot, frames=360, interval=20)
+plt.show()
+
+plt.show()
